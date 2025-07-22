3f534b928b9d6882f711c0ed0a1ab7f7c5dea55e
pratheekraj <prathik123gatty@gmail.com>
2025-07-22 13:22:29 +0530

    second commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Product extends Model
 {
     protected $fillable = ['title','description','image','price','category','quantity'];
-    
+    use Sluggable;
+    public function Sluggable(): array
+    {
+        return [
+            'slug'=>
+            [
+                'source'=>'title'
+            ]
+        ];
+    }
+
 }
